5cc971fc09659640503751bb3e59a7eb2a255d7e
Romir Mohan <romirmohan2028@u.northwestern.edu>
2025-10-26 19:30:30 -0500

    basic testing with pytest and how to initialize!

--- src/math_utils.py
+++ src/math_utils.py
@@ -1,2 +1,13 @@
-def multiply(a,b):
+def multiply(a, b):
     return a * b
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def divide (a, b):
+    if b == 0:
+        return ValueError
+    return a / b
