9bbdc88a04792f4e8750d7492112e12a0ee2a66e
Romir Mohan <romirmohan2028@u.northwestern.edu>
2025-10-26 20:01:11 -0500

    ruff formatting

--- src/math_utils.py
+++ src/math_utils.py
@@ -1,13 +1,16 @@
 def multiply(a, b):
     return a * b
 
+
 def add(a, b):
     return a + b
+
 
 def subtract(a, b):
     return a - b
 
-def divide (a, b):
+
+def divide(a, b):
     if b == 0:
-        return ValueError
+        raise ZeroDivisionError
     return a / b
--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -1,19 +1,27 @@
 from src.math_utils import multiply, add, subtract, divide
+import pytest
+
 
 def test_multiply_one():
     assert multiply(3, 4) == 12
 
+
 def test_multiply_two():
     assert multiply(4, 5) == 20
+
 
 def test_add_one():
     assert add(2, 3) == 5
 
+
 def test_subtract_one():
     assert subtract(2, 3) == -1
 
+
 def test_divide_one():
-    assert divide(3, 0) == ValueError
+    with pytest.raises(ZeroDivisionError):
+        divide(3, 0)
+
 
 def test_divide_two():
     assert divide(4, 1) == 4
